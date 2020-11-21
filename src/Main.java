ceb4e5a9ed631a75729eaacbfb8552be172d7e92
--replace <mail@yourmail.ru>
2020-11-21 20:28:53 +0300

    Branch3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import one.Cookie;
 
 public class Main {
     public static void main(String[] args) {
+        Cookie cookie22 = new Cookie();
+        Cookie cookie = new Cookie();
         Cookie cookie = new Cookie();
     }
 }
